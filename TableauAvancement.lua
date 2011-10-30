a96e1e42181d783bffd696574e5338d6cb9a9709
Adrien Bertrand <adrienbertrand@msn.com>
2011-10-30 20:44:49 +0100

    Equation + tableau

--- TableauAvancement.lua
+++ TableauAvancement.lua
@@ -13,8 +13,6 @@ device.api = platform.apilevel
 device.hasColor = platform.isColorDisplay()
 device.lang = locale.name()
 
-theError = ""
-
 -- End Globals --
 
 --BetterLuaAPI:--
@@ -63,33 +61,51 @@ function Tableau:init(data)
 	self.restes = data["restes"]
 	self.moles = data["moles"]
 	self.noms = data["noms"]
-	self.coeff = data["coeff"]
+	self.coeff = data["coeffs"]
 	self.xmax = data["xmax"]
 	self.reactLim = data["reactLim"]
+	self.nbrCol = self.nbrReact + self.nbrProd
+	self.equation = ""
+	for k,v in pairs(self.noms) do
+	    self.equation = self.equation .. tostring(self.coeff[k]) .. " " .. tostring(self.noms[k])
+	    if k ~= self.nbrReact then
+	        if k < self.nbrCol then self.equation = self.equation .. " + " end
+	    else
+	        self.equation = self.equation .. " -> "
+	    end
+	end
 	--------
 	self:resizeGC()
 end
 
 function Tableau:resizeGC()
-    self.nbrCol = self.nbrReact + self.nbrProd
 	self.xStart = .05*screenX
 	self.yStart = .15*screenY
 	self.width = .9*screenX
 	self.height = .8*screenY
-	self.xEnd = self.xStart + self.width
+	self.xEnd = self.xStart + self.width - 1
 	self.yEnd = self.yStart + self.height
-	self.stepX = (1/self.nbrCol)*self.width
+	self.stepX = (1/(1+self.nbrCol))*self.width
 	self.stepY = .25*self.height
+	self.txtSize = 9
 end
 
 function Tableau:paint(gc)
-	gc:drawRect(self.xStart,self.yStart,self.width,self.height)
-	for i=self.yStart+self.stepY,self.yEnd-self.stepY,self.stepY do
+	gc:drawRect(self.xStart,self.yStart,self.width,self.height+1)
+	for i=self.yStart+self.stepY,self.yEnd,self.stepY do
 	    gc:drawLine(self.xStart,i,self.xEnd,i)
 	end
-	for i=self.yStart+self.stepY,self.yEnd-self.stepY,self.stepY do
-	    gc:drawLine(self.xStart,i,self.xEnd,i)
+	gc:drawLine(self.xStart*1.65+self.stepX,self.yStart,self.xStart*1.65+self.stepX,self.yEnd-1)
+	gc:drawLine(self.xStart*1.52+(1+self.nbrReact)*self.stepX*1.005,self.yStart+self.stepY,self.xStart*1.52+(1+self.nbrReact)*self.stepX*1.005,self.yEnd-1)
+	for i=self.xStart*1.65+2*self.stepX,self.xEnd,0.95*self.stepX do
+	    gc:drawLine(i,self.yStart+self.stepY,i,self.yEnd-1)
 	end
+	self:paintTexts(gc)
+end
+
+function Tableau:paintTexts(gc)
+    gc:setFont("sansserif", "r", self.txtSize)
+    gc:drawString(self.equation,self.xStart+1.5*self.stepX,self.yStart,"top")
 end
 
 -- End Classes --
@@ -97,10 +113,15 @@ end
 --   Events:   --
 
 function on.create()
+    theError = ""
 	on.resize()
 	recupData() -- makes theData
 	tableau = Tableau(theData)
-	print("Done Init. Error : ", theError ~= "")
+	for k,v in pairs(tableau) do
+	    --print(k,v)
+	end
+	var.monitor("luasignal")
+	print("Done Init. Got Error ?", theError ~= "")
 end
 
 function on.resize()
@@ -112,41 +133,37 @@ function on.resize()
     if tableau then tableau:resizeGC() end
 end
 
+function on.charIn(key)
+    if key == "*" or key == "+" then print("plus") screenX = screenX + 25 screenY = screenY + 25 end
+    if key == "/" or key == "-" then screenX = screenX - 25 screenY = screenY - 25 end
+    if tableau then tableau:resizeGC() end
+    screenRefresh()
+end
+
 function on.paint(gc)
 	gc:setColorRGB(0,0,0)
-	--if theError then gc:setAlpha(128) end -- fond a moitié transparent
-	tableau:paint(gc)
+
+    tableau:paint(gc)
 	
-	if theError then
-	--	gc:setAlpha(255) -- affichage de l'erreur en non-transparent
-	
-		--Error handling code here....		
+	if theError ~= "" then
+	    print(theError)
+	    gc:drawString(theError,5,5,"top")
 	end
+end
+
+function on.varChange()
+    on.create()  -- reinitialisation du script.
 end
 
 --  End Events --
 
 --  Functions  --
 function recupData()
-	theData = { nbrReact = var.recall("nbrreact"), nbrProd = var.recall("nbrprod"), lNoms = var.recall("lnoms"), lCoeffs = var.recall("lcoeff"), lMoles = var.recall("lmoles"), lReste = var.recall("lreste"), reactLim = var.recall("reactlim"), xmax = var.recall("xmax") }
-	for _,v in pairs(theData) do
-		if not v then theError = "Veuillez exécuter le programme avancement() avant de consulter le tableau !" end
-	end
+	theData = { nbrReact = var.recall("nbrreact"), nbrProd = var.recall("nbrprod"), noms = var.recall("lnoms"), coeffs = var.recall("lcoeff"), moles = var.recall("lmoles"), restes = var.recall("lreste"), reactLim = var.recall("reactlim"), xmax = var.recall("xmax")}
+	if tonumber(theData["xmax"]) > 9000 or var.recall("luasignal") < 42 then theError = "Veuillez exécuter le programme avancement() avant de consulter le tableau !" else theError = "" end
 end
 
 --  End Func.  --
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
